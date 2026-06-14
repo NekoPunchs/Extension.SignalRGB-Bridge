9d03d23ad3fabb98c892f05ba6d735c640fc96b5
NekoPunch! <549643063@qq.com>
2026-06-15 02:56:49 +0800

    test

--- SONIX_Keyboard_Controller_2.js
+++ SONIX_Keyboard_Controller_2.js
@@ -132,32 +132,22 @@ export class SONIX_Device_Protocol {
 	}
 
 	ready_send(){
-		let packet = new Array(65).fill(0);
-		packet[1] = 0x04;
-		packet[2] = 0x20; // 0x00, 0x04, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08
-		packet[9] = 0x08;
-		device.send_report(packet, 65);
+		device.send_report([0x00, 0x04, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08], 65);
 		device.pause(delayMs);
-
-		packet.fill(0);
-		device.get_report(packet,65);
+		device.get_report([0x00],65);
 		device.pause(delayMs);
-		device.send_report(packet,1);
+		device.send_report([0x00],1);
 	}
 
 	refresh(){
-		let packet = new Array(65).fill(0);
-		device.send_report(packet, 65);
+		device.send_report([0x00], 65);
 		device.pause(delayMs);
-		device.send_report(packet, 65);
+		device.send_report([0x00], 65);
 		device.pause(delayMs);
 
-		packet[1] = 0x04; // [0x00, 0x04, 0x02]
-		packet[2] = 0x02;
-		device.send_report(packet, 65);
+		device.send_report([0x00, 0x04, 0x02], 65);
 		device.pause(delayMs);
-		packet.fill(0);
-		device.get_report(packet,65);
+		device.get_report([0x00],65);
 		device.pause(delayMs);
 	}
 
